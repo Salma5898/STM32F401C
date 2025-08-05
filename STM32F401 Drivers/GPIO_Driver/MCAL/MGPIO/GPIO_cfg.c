7b2e1a049a5fd61645e4f3a81d16d0f141f59c2a
Salma H.B. Khalafalla <139346316+Salma5898@users.noreply.github.com>
2025-08-05 03:56:54 -0800

    Add files via upload

---
+++ STM32F401 Drivers/GPIO_Driver/MCAL/MGPIO/GPIO_cfg.c
@@ -0,0 +1,73 @@
+/*
+ * GPIO_cfg.c
+ *
+ *  Created on: Jul 31, 2025
+ *      Author: samsung
+ */
+
+#include "../../LIB/BIT_MATH.h"
+#include "../../LIB/STD_TYPES.h"
+#include "../MRCC/RCC_init.h"
+
+#include "GPIO_init.h"
+#include "GPIO_private.h"
+#include "GPIO_reg.h"
+
+/*		User Configurations			*/
+
+GPIO_CFG_t Config_Output_Pin_Push_Pull =
+{
+		.Mode        = GPIO_MODE_OUTPUT               ,
+		.OutputSpeed = GPIO_SPEED_LOW     			,
+		.OutputType	 = GPIO_OutputType_PushPull		,
+		.InputPull	 = GPIO_NO_PULL
+};
+
+GPIO_CFG_t Config_Output_Pin_Open_Drain =
+{
+		.Mode         = GPIO_MODE_OUTPUT               ,
+		.OutputSpeed	 = GPIO_SPEED_LOW     			,
+		.OutputType	 = GPIO_OutputType_OpenDrain		,
+		.InputPull	 = GPIO_NO_PULL
+};
+
+GPIO_CFG_t Config_Input_Pin_No_Pull =
+{
+		.Mode         = GPIO_MODE_INPUT                ,
+		.OutputSpeed	 = GPIO_SPEED_LOW     			,
+		.OutputType	 = GPIO_OutputType_PushPull		,
+		.InputPull	 = GPIO_NO_PULL
+};
+
+GPIO_CFG_t Config_Input_Pin_Pull_Up =
+{
+		.Mode         = GPIO_MODE_INPUT                ,
+		.OutputSpeed	 = GPIO_SPEED_LOW     			,
+		.OutputType	 = GPIO_OutputType_PushPull		,
+		.InputPull	 = GPIO_PullUp
+
+};
+
+GPIO_CFG_t Config_Input_Pin_Pull_Down =
+{
+		.Mode        = GPIO_MODE_INPUT                ,
+		.OutputSpeed	= GPIO_SPEED_LOW     			,
+		.OutputType	= GPIO_OutputType_PushPull		,
+		.InputPull	= GPIO_PullDown
+};
+
+GPIO_CFG_t Config_Input_Analog =
+{
+		.Mode         = GPIO_MODE_ANALOG               ,
+		.OutputSpeed	 = GPIO_SPEED_LOW     			,
+		.OutputType	 = GPIO_OutputType_PushPull		,
+		.InputPull	 = GPIO_NO_PULL
+};
+
+GPIO_CFG_t Config_Pin_Alt =
+{
+		.Mode         = GPIO_MODE_ALF                 	,
+		.OutputSpeed	 = GPIO_SPEED_LOW    				,
+		.OutputType	 = GPIO_OutputType_PushPull		,
+		.InputPull	 = GPIO_NO_PULL
+};
